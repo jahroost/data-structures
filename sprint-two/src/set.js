82be799707b73d8364108b5bb8919193047fa1ea
Clayton Smart <claysmart@gmail.com>
2017-10-05 15:51:55 -0500

    finished set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,19 +1,34 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
-var setPrototype = {};
-
-setPrototype.add = function(item) {
+var setPrototype = {
+  add: function(item) {
+    this._storage.push(item);
+  },
+  contains: function(item) {
+    if (this._storage.includes(item)) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  remove: function(item) {
+    delete this._storage[0];
+  }
 };
 
-setPrototype.contains = function(item) {
-};
-
-setPrototype.remove = function(item) {
-};
+// setPrototype.add = function(item) {
+//   this._storage.push(item);
+// };
+//
+// setPrototype.contains = function(item) {
+// };
+//
+// setPrototype.remove = function(item) {
+// };
 
 /*
  * Complexity: What is the time complexity of the above functions?
