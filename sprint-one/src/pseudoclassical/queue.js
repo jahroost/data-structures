cc197232482c0749bb6c24e77b10518b2b13c9e3
Clayton Smart <claysmart@gmail.com>
2017-10-04 19:23:05 -0500

    finished pseudoclassical stack and queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,25 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.newestIndex = 0;
+  this.oldestIndex = 0;
+  this.storage = {};
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.newestIndex] = value;
+  this.newestIndex++;
+};
+Queue.prototype.dequeue = function() {
+  var result = this.storage[this.oldestIndex];
+  delete this.storage[this.oldestIndex];
+  this.oldestIndex++;
+  return result;
+};
+Queue.prototype.size = function() {
+  if((this.newestIndex - this.oldestIndex) < 0) {
+    return 0;
+  }
+  return this.newestIndex - this.oldestIndex;
+};
 
