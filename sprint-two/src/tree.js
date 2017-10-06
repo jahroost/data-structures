bcb1cf0b0b87ab0ec6f64a7fa6a16d65509365d9
Clayton Smart <claysmart@gmail.com>
2017-10-05 19:38:31 -0500

    working on last test, think i need to use recursion

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,28 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  var parent = null;
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var child = Tree(value);
+  child.parent = this;
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+  for (var i = 0; i < this.children.length; i++) {
+    if (this.children[i].value === target) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 };
 
 
