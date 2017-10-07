fa803b99743edfd0bd2fc14f25affbf1031ed2b9
Clayton Smart <claysmart@gmail.com>
2017-10-06 19:55:57 -0500

    started on hashTable.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,17 +7,19 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var val = getIndexBelowMaxForKey(v, this._limit);
   var bucketExists = !!this._storage.get(index);
 
   if (!bucketExists) {
-
+    this._storage.set(index, val);
   } else {
-
+    // this._storage.set(k, v);
   }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
