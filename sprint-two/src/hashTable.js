6eadce4a16b316282031421d29fa0463e2f7b54b
Clayton Smart <claysmart@gmail.com>
2017-10-07 22:02:11 -0500

    Finished hashTable.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,32 +1,95 @@
-
+//
+//
+// var HashTable = function() {
+//   this._limit = 8;
+//   this._storage = LimitedArray(this._limit);
+// };
+//
+// HashTable.prototype.insert = function(k, v) {
+//   var index = getIndexBelowMaxForKey(k, this._limit);
+//   // var val = getIndexBelowMaxForKey(v, this._limit);
+//   var bucketExists = this._storage.get[index];
+//
+//   if (!bucketExists) {
+//     var bucket = [];
+//     this._storage[index] = bucket;
+//   }
+//   for (var i = 0; i < bucket.length; i++) {
+//     var tuple = bucket[i];
+//     if (tuple[0] === k) {
+//       tuple[1] === v;
+//     }
+//   }
+// };
+//
+// HashTable.prototype.retrieve = function(k) {
+//   var index = getIndexBelowMaxForKey(k, this._limit);
+//   var bucket = this._storage[index];
+//
+//   for (var i = 0; i < bucket.length; i++) {
+//     var tuple = bucket[i];
+//     if (tuple[0] === k) {
+//       return tuple[1];
+//     }
+//   }
+// };
+//
+// HashTable.prototype.remove = function(k) {
+//   var index = getIndexBelowMaxForKey(k, this._limit);
+// };
 
 var HashTable = function() {
-  this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._limit = 8;
 };
 
-HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-  var val = getIndexBelowMaxForKey(v, this._limit);
-  var bucketExists = !!this._storage.get(index);
+HashTable.prototype.insert = function(key, value) {
+  var index = getIndexBelowMaxForKey(key, this._limit);
+  var bucket = this._storage[index];
+  if (!bucket) {
+    var bucket = [];
+    this._storage[index] = bucket;
+  }
 
-  if (!bucketExists) {
-    this._storage.set(index, val);
-  } else {
-    // this._storage.set(k, v);
+  var override = false;
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === key) {
+      tuple[1] = value;
+      override = true;
+    }
+  }
+
+  if (!override) {
+    bucket.push([key, value]);
+  }
+  this._storage.set(index, value);
+};
+
+HashTable.prototype.retrieve = function(key) {
+  var index = getIndexBelowMaxForKey(key, this._limit);
+  var bucket = this._storage[index];
+
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === key) {
+      return tuple[1];
+    }
   }
 };
 
-HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+HashTable.prototype.remove = function(key) {
+  var index = getIndexBelowMaxForKey(key, this._limit);
+  var bucket = this._storage[index];
+
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === key) {
+      bucket.splice(i, 1);
+      return tuple[1];
+    }
+  }
 };
-
-HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-};
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
